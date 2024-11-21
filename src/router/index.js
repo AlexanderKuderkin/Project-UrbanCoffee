fc16fa6fa48b78e036f9467411b30786449d43b5
al351kud <Alexander.Kuderkin@htwg-konstanz.de>
2024-11-21 11:28:47 +0100

    router installiert

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import NewCategoryView from "../views/NewCategoryView.vue";
+import AboutView from "../views/AboutView.vue";
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: HomeView,
+        },
+        {
+            path: "/new",
+            name: "new",
+            component: NewCategoryView,
+        },
+        {
+            path: "/about",
+            name: "about",
+            component: AboutView,
+        },
+    ],
+});
+
+export default router;
