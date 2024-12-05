4a13a410ef71df1971df7647504e9ffd7ce6b9e5
al351kud <Alexander.Kuderkin@htwg-konstanz.de>
2024-12-05 11:35:48 +0100

    localhost

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import axios from 'axios'; 
-axios.defaults.baseURL = "http://localhost:1337";
+axios.defaults.baseURL = "http://localhost:5173";
 
 const app = createApp(App);
 
