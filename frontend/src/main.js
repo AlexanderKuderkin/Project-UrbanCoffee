0630b08577ab2903bdd1e636948f22d9ca2fb469
al351kud <Alexander.Kuderkin@htwg-konstanz.de>
2024-12-05 11:26:46 +0100

    axios installiert

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import axios from 'axios'; 
+axios.defaults.baseURL = "http://localhost:1337";
 
 const app = createApp(App);
 
