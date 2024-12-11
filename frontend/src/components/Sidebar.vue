680fc95262e8531c46821f3c02a74fa32d4b145e
al351kud <Alexander.Kuderkin@htwg-konstanz.de>
2024-12-11 23:15:24 +0100

    new bransidebar hat jetzt auch ein "router-link"

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -16,8 +16,8 @@
     <div class="sidebar-body">
       <button class="btn btn-traditional">Sign up</button>
       <button class="btn btn-traditional">Login</button>
-      <button class="btn">Coffee</button>
-      <button class="btn">Admin</button>
+      <router-link to="/Coffee" class="btn">Coffee</router-link>
+      <router-link to="/ManageCoffee" class="btn">Manage Coffee</router-link>
     </div>
   </div>
 </template>
